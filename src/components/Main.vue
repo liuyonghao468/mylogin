3484a17c9075e3c1519d957d909fadfe59dd4b35
liuyonghao468 <799830524@qq.com>
2020-06-13 16:56:44 +0800

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+      <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to the Vue.js'
+    }
+  }
+}
+</script>
+
+<style>
+.main {
+    font-size:  14px;
+    color: #58666e;
+    background-color: #1c2b36
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+// 引入vue.js插件
+import VueRouter from 'vue-router'
+// 组件
+import Main from './components/Main.vue'
+import Login from './components/Login.vue'
+Vue.config.productionTip = false
+const routes = [
+  { name: 'main', path: '/', component: Main },
+  { name: 'login', path: '/login', component: Login }
+]
+var router = new VueRouter({
+  routes
+})
+Vue.use(VueRouter)
+var App = {
+  template: '<router-view></router-view>'
+}
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {
+    App
+  },
+  template: '<App/>'
+})
